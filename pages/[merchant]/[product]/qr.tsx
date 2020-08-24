dc0d88627930016a4ef538c484d519055bf9c5f4
thorwebdev <thorsten.schaeff@gmail.com>
2020-08-24 15:46:48 +0800

    feat: add QR code path.

--- pages/[merchant]/[product]/qr.tsx
+++ pages/[merchant]/[product]/qr.tsx
@@ -1,3 +1,14 @@
+import { useRouter } from "next/router";
+import QRCode from "qrcode.react";
+
 export default function QRCodePage() {
-  return <span>QR Code here</span>;
+  const router = useRouter();
+  const { merchant, product: productId } = router.query;
+  if (!productId) return <div>loading...</div>;
+  return (
+    <QRCode
+      includeMargin={true}
+      value={`${process.env.NEXT_PUBLIC_URL}/${merchant}/${productId}`}
+    />
+  );
 }
