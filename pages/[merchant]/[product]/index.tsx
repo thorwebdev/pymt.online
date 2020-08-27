780136a8b3cc041ecc19a91450006a9c7f8a971f
thorwebdev <thorsten.schaeff@gmail.com>
2020-08-28 00:39:26 +0800

    Move to ISG for product detail pages.

--- pages/[merchant]/[product]/index.tsx
+++ pages/[merchant]/[product]/index.tsx
@@ -1,35 +1,44 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
 
 export default function ProductPage({ product }) {
   const router = useRouter();
-  const { merchant, product: productId } = router.query;
-  const { data, error } = useSWR(
-    productId ? `/api/products/${merchant}/${productId}` : null,
-    async (url: string) => await fetch(url).then((res) => res.json())
-  );
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) return <div>Loading...</div>;
+  if (!product || product.error) return <div>failed to load</div>;
+  return <pre>{JSON.stringify(product, null, 2)}</pre>;
 }
 
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// revalidation is enabled and a new request comes in
-// export async function getServerSideProps() {
-//   const router = useRouter();
-//   const { merchant, product: productId } = router.query;
-//   const res = await fetch(`/api/products/${merchant}/${productId}`);
-//   const product = await res.json();
+// This function gets called at build time
+export async function getStaticPaths() {
+  return {
+    // Only `/posts/1` and `/posts/2` are generated at build time
+    paths: [],
+    // Enable statically generating additional pages
+    // For example: `/posts/3`
+    fallback: true,
+  };
+}
 
-//   return {
-//     props: {
-//       product,
-//     },
-//     // Next.js will attempt to re-generate the page:
-//     // - When a request comes in
-//     // - At most once every second
-//     // revalidate: 1, // In seconds
-//   };
-// }
+// This also gets called at build time
+export async function getStaticProps({
+  params,
+}: {
+  params: { merchant: string; product: string };
+}) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(
+    `${process.env.URL}/api/products/${params.merchant}/${params.product}`
+  );
+  const product = await res.json();
+
+  // Pass post data to the page via props
+  return {
+    props: { product },
+    // Re-generate the post at most once per second
+    // if a request comes in
+    revalidate: 60,
+  };
+}
