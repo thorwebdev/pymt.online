f6b7e8736fc9d44bc99f70e93dac61467c96d0ba
thorwebdev <thorsten.schaeff@gmail.com>
2020-09-04 00:59:50 +0800

    feat: Handling Stripe Connect Webhooks.

---
+++ pages/api/webhooks/connect.ts
@@ -0,0 +1,66 @@
+import { buffer } from "micro";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2020-03-02",
+});
+
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
+
+// Stripe requires the raw body to construct the event.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"]!;
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf.toString(),
+        sig,
+        webhookSecret
+      );
+    } catch (err) {
+      // On error, log and return the error message.
+      console.log(`❌ Error message: ${err.message}`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Successfully constructed event.
+    console.log("✅ Success:", event.id);
+
+    // Cast event data to Stripe object.
+    if (event.type === "product.created") {
+      const product = event.data.object as Stripe.Product;
+      const pymtLink = `${process.env.URL}/${event.account}/${product.id}`;
+      const qrCodeLink = `${process.env.URL}/${event.account}/${product.id}/qr`;
+      await stripe.products.update(
+        product.id,
+        {
+          metadata: { pymtLink, qrCodeLink },
+        },
+        { stripeAccount: event.account }
+      );
+      console.log(`Product [${product.id}] has been updated.`);
+    } else {
+      // console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event.
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+};
+
+export default webhookHandler;
