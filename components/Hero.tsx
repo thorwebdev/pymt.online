77bd11f98d70a04facd1d6d60e0a23c38cdb8f03
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-09-20 03:29:58 +0800

    next: Powered by Stripe.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,7 @@ export default function Hero({ size }: { size?: string }) {
       <Box bg="secondary" w="full" h="5px" />
       <Flex
         as="nav"
+        wrap="wrap"
         flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
@@ -26,18 +27,21 @@ export default function Hero({ size }: { size?: string }) {
           <Heading as="h1" size="2xl" color="white">
             pymt.online
           </Heading>
-          <Heading as="h2" size="lg" color="white">
+          <Heading as="h2" size="lg" color="white" mt="24px">
             The fastest way to start selling online with Stripe.
           </Heading>
-          {size !== "small" ? (
-            <NextLink href="/api/accounts/link" passHref>
-              <Button as="a" size="lg" bg="secondary" mt="24px">
-                Get started
-              </Button>
-            </NextLink>
-          ) : (
-            ""
-          )}
+          <Flex align="center" wrap="wrap">
+            {size !== "small" ? (
+              <NextLink href="/api/accounts/link" passHref>
+                <Button as="a" size="lg" bg="secondary" mt="24px" mr={4}>
+                  Get started
+                </Button>
+              </NextLink>
+            ) : (
+              ""
+            )}
+            <Image maxHeight="48px" src="/pbsw.svg" mt="24px" />
+          </Flex>
         </Box>
       </Flex>
     </Box>
